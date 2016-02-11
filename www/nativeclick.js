02ff82c865f4690381108aa7ed1c4f4047aa8844
swtest13 <ygeva@casebank.com>
2016-02-11 11:35:55 -0500

    Update nativeclick.js

--- www/nativeclick.js
+++ www/nativeclick.js
@@ -1,27 +1,11 @@
 var exec = require('cordova/exec');
 
 var nativeclick = {
-
-  watch: function(classes){
-
-    var clickyClasses = classes || [];
-
-    for(var i = 0; i < clickyClasses.length; i++) {
-      watchClass(clickyClasses[i]);
-    }
-
-    function watchClass(name){
-      document.body.addEventListener( 'click' , function(e) {
-        e = window.event? event.srcElement: e.target;
-        if(e.className && e.className.indexOf(name) != -1) {
-          exec(null, errorCallback, "NativeClick", "click", []);
-        }
-      });
-    }
-   
-    var errorCallback = function(error){
+  trigger: function () {
+     var errorCallback = function(error){
       console.error("Native Click Error:"+error);
     };
+    exec(null, errorCallback, "NativeClick", "click", []);
   }
 };
 
